b38c7c9f5a76de8ab0de40641cc9e6085a62978d
lucapiccinelli <luca.picci@gmail.com>
2019-01-13 16:01:41 +0100

    starting working folder

---
+++ tests/AsAPlayer_IWantToKnow_MyScore.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace tests
+{
+    public class AsAPlayer_IWantToKnow_MyScore
+    {
+        [Fact]
+        public void RunningTheProgram_WithGivenInputString_WillWriteTheOutput_InTheConsole()
+        {
+
+        }
+    }
+}
