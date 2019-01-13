46cdd573947ba34a0d6caef1e565761217777327
lucapiccinelli <luca.picci@gmail.com>
2019-01-13 18:13:24 +0100

    first acceptance test, all open frames sums to 20

--- app/Program.cs
+++ app/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Linq;
 
 namespace app
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Split frames
+            string[] frames = args[1].Split(",");
+
+            // Split and convert each roll in order to have partial score of each frame
+            int[] partialScores = frames
+                .Select(frameStr => frameStr.Split("-"))
+                .Select(rolls => rolls.Select(roll => Convert.ToInt32(roll)).Sum())
+                .ToArray();
+
+            int totalScore = partialScores.Sum();
+
+            Console.WriteLine(args[1]);
+            Console.WriteLine(partialScores
+                .Aggregate("", (acc, score) => $"{acc}{(acc == string.Empty ? string.Empty : ",")}{score}"));
+            Console.WriteLine(totalScore);
         }
     }
 }
--- tests/AsAPlayer_IWantToKnow_MyScore.cs
+++ tests/AsAPlayer_IWantToKnow_MyScore.cs
@@ -1,14 +1,34 @@
 using System;
+using System.IO;
 using Xunit;
+using app;
 
 namespace tests
 {
     public class AsAPlayer_IWantToKnow_MyScore
     {
-        [Fact]
-        public void RunningTheProgram_WithGivenInputString_WillWriteTheOutput_InTheConsole()
+        [Theory]
+        [InlineData("1-1,1-1,1-1,1-1,1-1,1-1,1-1,1-1,1-1,1-1", "2,2,2,2,2,2,2,2,2,2", "20")] // All open frames
+        public void RunningTheProgram_WithGivenInputString_WillWriteTheOutput_InTheConsole(string rolls, string expectedScoreByFrame, string expectedTotalScore)
         {
+            using(StringWriter sw = new StringWriter())
+            {
+                // We set this StringWriter as Console output so that we can test the output
+                Console.SetOut(sw);
+                Program.Main(new[]{"-r", rolls});
+                string consoleOut = sw.ToString();
+                string[] lines = consoleOut.Split(Environment.NewLine);
 
+                //Assert
+                Assert.Equal(4, lines.Length);
+                string outRolls = lines[0];
+                string outScoreByFrame = lines[1];
+                string outTotalScore = lines[2];
+
+                Assert.Equal(rolls, outRolls);
+                Assert.Equal(expectedScoreByFrame, outScoreByFrame);
+                Assert.Equal(expectedTotalScore, outTotalScore);
+            }
         }
     }
 }
